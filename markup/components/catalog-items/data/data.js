ab0ce21908a723236f3031860c976cd19184de4c
Vladimir-Sartakov <vov.sartakov@mail.ru>
2017-03-26 21:07:10 +0300

    main page: catalog items and footer

---
+++ markup/components/catalog-items/data/data.js
@@ -0,0 +1,65 @@
+catalogItemsData: {
+    goods: [
+        {
+            img: '/static/img/content/product-no-fhoto.jpg',
+            title: 'Самый клевый чайник, который надо купить!',
+            href: 'javascript:void(0);',
+            price: '1900 р.'
+        },
+        {
+            img: '/static/img/content/product-no-fhoto.jpg',
+            title: 'Самый клевый чайник, который надо купить!',
+            href: 'javascript:void(0);',
+            price: '1900 р.'
+        },
+        {
+            img: '/static/img/content/product-no-fhoto.jpg',
+            title: 'Самый клевый чайник, который надо купить!',
+            href: 'javascript:void(0);',
+            price: '1900 р.'
+        },
+        {
+            img: '/static/img/content/product-no-fhoto.jpg',
+            title: 'Самый клевый чайник, который надо купить!',
+            href: 'javascript:void(0);',
+            price: '1900 р.'
+        },
+        {
+            img: '/static/img/content/product-no-fhoto.jpg',
+            title: 'Самый клевый чайник, который надо купить!',
+            href: 'javascript:void(0);',
+            price: '1900 р.'
+        }    
+    ],
+    pages:[
+        {
+            title: 1,
+            href: 'javascript:void(0);'
+        },
+        {
+            title: 2,
+            href: 'javascript:void(0);'
+        },
+        {
+            title: 3,
+            href: 'javascript:void(0);',
+            active: true
+        },
+        {
+            title: 4,
+            href: 'javascript:void(0);'
+        },
+        {
+            title: 5,
+            href: 'javascript:void(0);'
+        },
+        {
+            title: 6,
+            href: 'javascript:void(0);'
+        },
+        {
+            title: 7,
+            href: 'javascript:void(0);'
+        }                                
+    ]
+}
---
+++ markup/components/footer/data/data.js
@@ -0,0 +1,26 @@
+footerData: {
+    links: [
+        {
+            title: 'Главная',
+            href: 'javascript:void(0);'
+        },
+        {
+            title: 'Каталог',
+            href: 'javascript:void(0);'
+        },
+        {
+            title: 'Доставка и оплата',
+            href: 'javascript:void(0);'
+        },
+        {
+            title: 'Прайс-лист',
+            href: 'javascript:void(0);'
+        },
+        {
+            title: 'Контакты',
+            href: 'javascript:void(0);'
+        }
+    ],   
+    email: 'info@name.ru',
+    copyRight: '© 2012–2013 ЗАО «Компания»',
+}
